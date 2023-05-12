d114e912226eaae00ccad304e8595aff1b701537
JuanJacoboViera <juanjacoboviera@gmail.com>
2023-05-12 16:52:09 -0500

    add data.js mock service

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './style.css';
 import Nav from './components/nav/Nav'
 import RegistroCiudadanos from './pages/moduloRegistroCiudadanos/RegistroCiudadanos'
+import RegistroVacantes from './pages/moduloRegistroVacantes/RegistroVacantes'
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
     <Nav></Nav>
    </header>
    <main>
-   <RegistroCiudadanos></RegistroCiudadanos>
+   {/* <RegistroCiudadanos></RegistroCiudadanos> */}
+   <RegistroVacantes></RegistroVacantes>
 
    </main>
    </>
---
+++ src/data.js
@@ -0,0 +1,51 @@
+export const jobOffers = [
+    {
+        codigo: 'RS001',
+        cargo: 'Ingeniero Industrial',
+        descripcion: 'Se requiere Ingeniero Industrial con mínimo 2 años de experiencia en Salud Ocupacional',
+        empresa: 'EPM',
+        salario: 2500000
+    },
+    {
+        codigo: 'RS002',
+        cargo: 'Profesor de Química',
+        descripcion: 'Se requiere',
+        empresa: 'Institución educativa IES',
+        salario: 1900000
+    },
+    {
+        codigo: 'RS003',
+        cargo: 'Ingeniero de Desarrollo Junior',
+        descripcion: 'Se requiere',
+        empresa: 'XRM Services',
+        salario: 2600000
+    },
+    {
+        codigo: 'RS004',
+        cargo: 'Coordinador de Mercadeo',
+        descripcion: 'Se requiere',
+        empresa: 'Insercol',
+        salario: 1350000
+    },
+    {
+        codigo: 'RS005',
+        cargo: 'Profesor de Matemáticas',
+        descripcion: 'Se requiere',
+        empresa: 'SENA',
+        salario: 1075000
+    },
+    {
+        codigo: 'RS006',
+        cargo: 'Mensajero',
+        descripcion: 'Se requiere',
+        empresa: 'SERVIENTREGA',
+        salario: 950000
+    },
+    {
+        codigo: 'RS007',
+        cargo: 'Cajero',
+        descripcion: 'Se requiere',
+        empresa: 'Almacenes Éxito',
+        salario: 850000
+    },
+]
